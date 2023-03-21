1202ceb8230476f1c3394735447b9b9bbc3a4dd2
Bercove <bercove@gmail.com>
2023-03-21 04:59:40 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,17 +3,9 @@
  * print_times_table - prints out the n times table, starting with 0
  * @n: times table
  */
-void print_times_table(int n)
-{
-int x, y, z, k, t;
-
-	for (x = 0; x <= n; x++)
-	{
-		for (y = 0; y <= n; y++)
-		{
-			z = x * y;
-			if (z == 0 && y == 0)
-				_putchar(z + '0');
+void print_times_table(int n){ int x, y, z, k, t;
+	for (x = 0; x <= n; x++){		for (y = 0; y <= n; y++){			z = x * y;
+			if (z == 0 && y == 0) _putchar(z + '0');
 			else if (z > 99)
 			{
 				k = z % 10;
@@ -23,28 +15,16 @@ int x, y, z, k, t;
 				_putchar(' ');
 				_putchar(z + '0');
 				_putchar(t + '0');
-				_putchar(k + '0');
-			}
-			else if (z > 9)
-			{
+				_putchar(k + '0');}else if (z > 9){
 				k = z % 10;
 				z /= 10;
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(z + '0');
-				_putchar(k + '0');
-
-			}
-			else
-			{
+			_putchar(k + '0');}else			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(z + '0');
-			}
-		}
-		_putchar('\n');
-	}
-}
+				_putchar(z + '0');}}		_putchar('\n');}}
